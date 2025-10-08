d2e72195f70030d0d121586a7786bf76c6f38fa8
Christoph Schneider <Christoph.Schneider@dci-student.org>
2025-10-08 10:00:48 +0200

    fix: models.py changed food class

--- foods/models.py
+++ foods/models.py
@@ -3,4 +3,10 @@ from django.db import models
 
 class Food(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    def to_dictionary(self):
+        return {
+            'name': self.name,
+            'description': self.description
+        }
